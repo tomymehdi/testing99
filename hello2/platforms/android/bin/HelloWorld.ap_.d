c4ec23a01331eeaeaa4793584b81c7ce80618619
Tomas <tomymehdi@gmail.com>
2014-01-08 17:15:26 +0100

    del

--- hello2/platforms/android/bin/HelloWorld.ap_.d
+++ hello2/platforms/android/bin/HelloWorld.ap_.d
@@ -1,15 +1,29 @@
 /Users/Tom/appss/testing99/hello2/platforms/android/bin/HelloWorld.ap_ \
- : /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/icon.png \
+ : /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/facebook_icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-mdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-xhdpi/close.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/values/strings.xml \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/xml/config.xml \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-hdpi/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-hdpi/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-hdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-ldpi/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-ldpi/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-ldpi/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-mdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-xhdpi/close.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/bin/res/drawable-xhdpi/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/config.xml \
@@ -21,6 +35,7 @@
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/img \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/index.html \
+/Users/Tom/appss/testing99/hello2/platforms/android/assets/www/index3.html \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/js \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/res \
 /Users/Tom/appss/testing99/hello2/platforms/android/assets/www/spec \
--- hello2/platforms/android/gen/R.java.d
+++ hello2/platforms/android/gen/R.java.d
@@ -1,8 +1,15 @@
 /Users/Tom/appss/testing99/hello2/platforms/android/gen/io/cordova/hellocordova/R.java \
- : /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/icon.png \
+ : /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/facebook_icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/close.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/facebook_icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-mdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-xhdpi/close.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/values/strings.xml \
 /Users/Tom/appss/testing99/hello2/platforms/android/res/xml/config.xml \
