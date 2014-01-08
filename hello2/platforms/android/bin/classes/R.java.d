afc5c6e4391fb124ccf78a5ef4d7c5e6ef400f94
Tomas <tomymehdi@gmail.com>
2014-01-08 17:05:35 +0100

    before testing it on the phone

---
+++ hello2/platforms/android/bin/classes/R.java.d
@@ -0,0 +1,9 @@
+/Users/Tom/appss/testing99/hello2/platforms/android/gen/io/cordova/hellocordova/R.java \
+ : /Users/Tom/appss/testing99/hello2/platforms/android/res/drawable/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-hdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-ldpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-mdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/values/strings.xml \
+/Users/Tom/appss/testing99/hello2/platforms/android/res/xml/config.xml \
+/Users/Tom/appss/testing99/hello2/platforms/android/bin/AndroidManifest.xml \
